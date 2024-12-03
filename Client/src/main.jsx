398e34f91a86bce5731426a5a98be7d93f25784c
nitesh11-dk <nittesh.dk@gmail.com>
2024-12-03 21:39:34 +0530

    last try

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Appstate from './context/Appstate';  
-
+import AppState from './context/Appstate.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Appstate>
+  <AppState>
     <App />
-  </Appstate>,
+  </AppState>,
 )
