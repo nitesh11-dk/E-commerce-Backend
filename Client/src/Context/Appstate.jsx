0876dc535f14f638d1b73cf37959affec8df9acd
nitesh11-dk <nittesh.dk@gmail.com>
2024-12-03 21:10:08 +0530

    done for appstate

--- Client/src/Context/Appstate.jsx
+++ Client/src/Context/Appstate.jsx
@@ -1,5 +1,5 @@
   import { useEffect, useState } from "react";
-  import AppContext from "./Appcontext";
+  import AppContext from "./AppContext.jsx";
   import axios from "axios";
 import { toast } from "react-toastify";
 import { BASE_URL } from "../constants/config.js";
