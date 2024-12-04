37bf5c0441d6f0df51f9aaa5122b677af289371f
nitesh11-dk <nittesh.dk@gmail.com>
2024-12-04 13:48:56 +0530

    done .jsx removecd

--- Client/src/Context/Appstate.jsx
+++ Client/src/Context/Appstate.jsx
@@ -1,5 +1,5 @@
   import { useEffect, useState } from "react";
-  import AppContext from "./AppContext.jsx";
+  import AppContext from "./AppContext";
   import axios from "axios";
 import { toast } from "react-toastify";
 import { BASE_URL } from "../constants/config.js";
